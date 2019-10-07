78ed0708c04cd6a8a914f6cb030356ce2b94a0a5
miracle <miracle@mh100031862.corp.miraclesoft.com>
2019-10-07 15:13:41 +0530

    Navigation

--- src/app/app-info/app-info.component.ts
+++ src/app/app-info/app-info.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material';
 import {MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-app-info',
@@ -12,7 +13,7 @@ export class AppInfoComponent implements OnInit {
 app;
   ngOnInit() {
   }
-  constructor(private _bottomSheetRef: MatBottomSheetRef<AppInfoComponent>, @Inject(MAT_BOTTOM_SHEET_DATA) public data: any) { 
+  constructor( private route : Router, private _bottomSheetRef: MatBottomSheetRef<AppInfoComponent>, @Inject(MAT_BOTTOM_SHEET_DATA) public data: any) { 
     console.log(data, 'recievedData');
     this.app = data;
   }
@@ -26,7 +27,13 @@ app;
     console.log('update clicked');
   }
   installApp(id) {
-    console.log('install clicked');
+    
+    console.log('install clicked', id);
+    if(id == 3){
+      this._bottomSheetRef.dismiss();
+
+    this.route.navigateByUrl('/dashboard')
+    }
     this.install = !this.install
   }
   uninstallApp(id) {
