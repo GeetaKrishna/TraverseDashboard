c77024d2b21ea341c5921df01c07e78d6d4b08d9
Harish Madugula <miracle@mh100031862.corp.miraclesoft.com>
2019-10-10 18:01:17 +0530

    Added Routes in AppStore

--- src/app/app-info/app-info.component.ts
+++ src/app/app-info/app-info.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatBottomSheetRef } from '@angular/material';
-import {MAT_BOTTOM_SHEET_DATA} from '@angular/material/bottom-sheet';
+import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,10 +10,10 @@ import { Router } from '@angular/router';
 })
 export class AppInfoComponent implements OnInit {
   install: boolean = false
-app;
+  app;
   ngOnInit() {
   }
-  constructor( private route : Router, private _bottomSheetRef: MatBottomSheetRef<AppInfoComponent>, @Inject(MAT_BOTTOM_SHEET_DATA) public data: any) { 
+  constructor(private route: Router, private _bottomSheetRef: MatBottomSheetRef<AppInfoComponent>, @Inject(MAT_BOTTOM_SHEET_DATA) public data: any) {
     console.log(data, 'recievedData');
     this.app = data;
   }
@@ -27,12 +27,19 @@ app;
     console.log('update clicked');
   }
   installApp(id) {
-    
     console.log('install clicked', id);
-    if(id == 3){
+    if (id == 2) {
       this._bottomSheetRef.dismiss();
-
-    this.route.navigateByUrl('admin/dashboard')
+      this.route.navigateByUrl('admin/fitnessTracker/fitbit')
+    } else if (id == 3) {
+      this._bottomSheetRef.dismiss();
+      this.route.navigateByUrl('admin/dashboard')
+    } else if (id == 5) {
+      this._bottomSheetRef.dismiss();
+      this.route.navigateByUrl('admin/calendar')
+    } else if (id == 7) {
+      this._bottomSheetRef.dismiss();
+      this.route.navigateByUrl('admin/medication')
     }
     this.install = !this.install
   }
