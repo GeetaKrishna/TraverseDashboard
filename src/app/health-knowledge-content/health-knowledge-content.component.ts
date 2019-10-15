ad5505d5f92d6c5da66d2983c9e6d92c52fcf45e
Harish Madugula <miracle@mh100031862.corp.miraclesoft.com>
2019-10-15 19:48:04 +0530

    KnwoledgeBase Cards

--- src/app/health-knowledge-content/health-knowledge-content.component.ts
+++ src/app/health-knowledge-content/health-knowledge-content.component.ts
@@ -11,15 +11,47 @@ export class HealthKnowledgeContentComponent implements OnInit {
 
   ngOnInit() {
   }
+  apps: any[] = [
+    {
+      "appId": 1,
+      "appTitle": "Top 10 Natural options for Gout Treatment",
+      "appContent": " The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.",
+      "url": "assets/icons-healthKnowledge/honey.jpg",
+    },
+    {
+      "appId": 2,
+      "appTitle": "Study Finds Best Exercise Routine to Strengthen your Heart",
+      "appContent": " The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.",
+      "url": "assets/icons-healthKnowledge/heart.jpeg",
+    },
+    {
+      "appId": 3,
+      "appTitle": "FDA Issues Warning About Popular Blood Pressure Drug",
+      "appContent": " The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.",
+      "url": "assets/icons-healthKnowledge/BloodPressure.jpg",
+    },
+    {
+      "appId": 4,
+      "appTitle": "What is the Best Sleeping Position By Age",
+      "appContent": " The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog from Japan.A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was originally bred for hunting.",
+      "url": "assets/icons-healthKnowledge/baby.jpg",
+    }
+  ]
+
   searchVal: string = '';
 
   searchValue(searchVal) {
-
     console.log('searchValue', searchVal);
-
   }
   clearValue() {
     this.searchVal = ''
     console.log('clear val', this.searchValue);
   }
+  shareCard(id) {
+    console.log('share Clicked', id);
+  }
+  shareMessage(id) {
+    console.log('msg Clicked', id);
+  }
+
 }
